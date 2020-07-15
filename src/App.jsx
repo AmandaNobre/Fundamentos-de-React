5cda554014b249595c50e0331cd6bebf40a6f6f8
AmandaNobre <amandanobree71@gmail.com>
2020-07-15 16:31:35 -0300

    Usando .map e cloneElement

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,40 @@ import Fragmento from './components/basicos/Fragmentos'
 import Aleatorio from './components/basicos/Aleatorio'
 import Card from './components/basicos/layout/Card'
 import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 
 export default _ =>
     <div className="App">
         <h1>Fundamentos React (Arrow) </h1>
         <div className="Cards">
             <Card titulo="#05 Componente com filhos" color="#00CED1">
-                <Familia
-                    sobrenome ="Silva"
-                />
+                <Familia sobrenome="Silva">
+                    <FamiliaMembro nome="Amanda"/>
+                    <FamiliaMembro nome="Ana"/>
+                </Familia>
+                    
             </Card>
 
-            <Card titulo="#04 Desafio Aleatório" color="#40E0D0">
-                <Aleatorio
-                    min={1}
-                    max={10}
-                />
-            </Card>
+                <Card titulo="#04 Desafio Aleatório" color="#40E0D0">
+                    <Aleatorio
+                        min={1}
+                        max={10}
+                    />
+                </Card>
 
-            <Card titulo="#03 Fragmento" color="#48D1CC">
-                <Fragmento />
-            </Card>
+                <Card titulo="#03 Fragmento" color="#48D1CC">
+                    <Fragmento />
+                </Card>
 
-            <Card titulo="#02 Com Parâmetro" color="#20B2AA">
-                <ComParametro
-                    titulo="Dados do aluno"
-                    aluno="Thiago"
-                    nota={9.9} />
-            </Card>
+                <Card titulo="#02 Com Parâmetro" color="#20B2AA">
+                    <ComParametro
+                        titulo="Dados do aluno"
+                        aluno="Thiago"
+                        nota={9.9} />
+                </Card>
 
-            <Card titulo="#01 Primeiro COmponente" color="#008B8B">
-                <Primeiro></Primeiro>
-            </Card>
+                <Card titulo="#01 Primeiro COmponente" color="#008B8B">
+                    <Primeiro></Primeiro>
+                </Card>
         </div>
-    </div>
+        </div>
--- src/components/basicos/Familia.jsx
+++ src/components/basicos/Familia.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import FamiliaMembro from './FamiliaMembro'
+import React, { cloneElement } from 'react'
 
 export default props => {
     return (
         <div>
-            <FamiliaMembro nome="Amanda" sobrenome={props.sobrenome}/>
-            <FamiliaMembro nome="Amanda" {...props}/>
+            {
+                props.children.map((child, i) => {
+                    return cloneElement(child, {...props, Key: i})
+                })
+            }
         </div>
     )
 }
