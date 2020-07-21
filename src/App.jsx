49f283633e74e69477a778b6038c32739c24f611
AmandaNobre <amandanobree71@gmail.com>
2020-07-21 15:59:40 -0300

    Comunicação Indireta

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,20 @@ import TabelaProdutos from './components/repeticao/TabelaProdutos'
 import ParOuImpar from './components/condicional/ParOuImpar'
 import UsuarioInfo from './components/condicional/UsuarioInfo'
 import DiretaPai from './components/comunicacao/DiretaPai'
+import IndiretaPai from './components/comunicacao/IndiretaPai.jsx'
 
 export default _ =>
     <div className="App">
         <h1>Fundamentos React (Arrow) </h1>
         <div className="Cards">
-            <Card titulo="#09 Comunicação direta" color="#00FFFF">
-                <DiretaPai/>
+            <Card titulo="#10 Comunicação indireta" color="#00FFFF">
+                <IndiretaPai />
             </Card>
+
+            <Card titulo="#09 Comunicação direta" color="#00FFFF">
+                <DiretaPai />
+            </Card>
+
             <Card titulo="#08 Impar ou Par" color="#00FFFF">
                 <ParOuImpar
                     numero={2}
