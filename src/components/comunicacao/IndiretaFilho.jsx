f1c4744f3aa718df81334a9f654fe76cdbf08994
AmandaNobre <amandanobree71@gmail.com>
2020-07-21 16:02:39 -0300

    Correção de erro

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default props => {
-    const gerarIdade = () => parseInt(Math.rando() * (20)) + 50
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50
     const gerarNerd = () => Math.random() > 0.5
     return (
         <div>
